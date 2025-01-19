b436963f4a3143d7ced38ba96a17b39f3ff5a6da
duridev <duridev@outlook.com>
2025-01-19 02:13:28 -0300

    creacion de constantes

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+const amigo = document.querySelector('.input-name');
+const listaAmigos = document.querySelector('#listaAmigos')
+const resultado = document.querySelector('#resultado')
+
